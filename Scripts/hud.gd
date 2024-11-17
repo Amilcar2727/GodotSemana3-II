c57e6dd6d38fb70eded544a4b580a6b78051c366
YoNi~ <67379339+Amilcar2727@users.noreply.github.com>
2024-11-17 11:51:36 -0500

    Add files via upload

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,29 +1,43 @@
 extends CanvasLayer
-
+# Declaramos la variable que iniciará 
+# el juego al presionar el boton start.
 signal start_game;
 
 func show_message(text):
+	#Asignamos a la propiedad text del nodo Message el parámetro
 	$Message.text = text;
+	#Mostramos el nodo en caso esté oculto
 	$Message.show();
+	#Iniciamos el timer (2s)
 	$MessageTimer.start();
-
 func update_score(score):
+	#Asignamos a la propiedad text del nodo ScoreLabel el parametro
+	#convertido en string (str)
 	$ScoreLabel.text = str(score);
 
 func show_game_over():
+	#Si perdemos, cambiamos el nodo Mensaje
 	show_message("Game Over");
+	#Esperamos a que se envie la señal timeout del nodo MessageTimer
+	#Inicializado en el metodo show_message(text) 2s.
 	await $MessageTimer.timeout;
-	# STOP
+	#Cambiamos el mensaje del nodo Message
 	$Message.text = "Dodge the Creeps!";
+	#Mostramos en caso este oculto
 	$Message.show();
-	
+	#Creamos un timer adicional de 1 segundo, y esperaremos que envie su
+	#señal timeout. 
 	await get_tree().create_timer(1.0).timeout;
+	#Mostramos el boton caso este oculto
 	$StartButton.show();
-	
 
+# Cuando se presione el boton
 func _on_start_button_pressed():
+	# Lo ocultamos
 	$StartButton.hide();
+	# Emitimos la señal start_game
 	start_game.emit();
 
 func _on_message_timer_timeout():
+	# Despues de 2 segundos, ocultamos el nodo Message
 	$Message.hide();
--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,25 +5,40 @@ extends Node
 
 var score;
 
-func _ready():
-	#new_game();
-	pass;
-# FUNCIONAR CUANDO NOS COLISIONE ENEMIGO
+# FUNCIONA CUANDO NOS COLISIONE ENEMIGO
 func game_over():
 	$ScoreTimer.stop(); # DETENER LA PUNTUACION
 	$MobTimer.stop(); # DETENEMOS EL SPAWN DE ENEMIGO
+	$HUD.show_game_over(); #MOSTRAR GAME OVER
+	$Music.stop(); 
+	$DeathSound.play();
 
 func new_game():
+	# Llamamos al grupo del arbol de nodos, y los hacemos eliminar
+	get_tree().call_group("mobs","queue_free");
+	# Inicializamos la puntuación
 	score = 0;
+	# La mostramos haciendo uso del HUD
+	$HUD.update_score(score);
+	# Mostramos el mensaje de preparación con el HUD
+	$HUD.show_message("Get Ready!");
+	# Hacemos aparecer al jugador en una posicion dada
 	$Player.start($StartPosition.position);
+	# Inicializamos el timer que empieza el juego.
 	$StartTimer.start();
+	# Inicializamos la musica de fondo.
+	$Music.play();
 
 func _on_start_timer_timeout():
 	$MobTimer.start();
 	$ScoreTimer.start();
 
+# Se ejecuta cada 1 Segundo
 func _on_score_timer_timeout():
+	# Aumenta la puntuacion c/segundo
 	score += 1;
+	# Lo muestra en pantalla con el HUD
+	$HUD.update_score(score);
 	
 func _on_mob_timer_timeout(): #0.5 segundos
 	#Mob -> Clase, Instanciarlo -> Nuevo objeto; (COPIA)
@@ -39,6 +54,7 @@ func _on_mob_timer_timeout(): #0.5 segundos
 	mob.position = mob_spawn_location.position;
 	
 	# DANDO LA DIRECCION A NUESTRO ENEMIGO PARA QUE APUNTE PANTALLA
+	direction += randf_range(-PI / 4, PI / 4);
 	mob.rotation = direction;
 	#                        ======= x =========== , == y ==
 	var velocity = Vector2(randf_range(150.0, 250.0),0);
